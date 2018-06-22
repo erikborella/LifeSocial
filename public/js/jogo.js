1ee48a1bd758087dd60b78c90c49894bf9a00468
ErikIPU2 <erik02_palmeiras@hotmail.com>
2018-06-22 13:54:48 -0300

    Ajeitada tela de jogo

--- public/js/jogo.js
+++ public/js/jogo.js
@@ -6,6 +6,10 @@ function initGame(indexUser) {
 
 	$(".gameTela").show();
 
-	
-	
+	$.get("/getUserdata", (data, status) => {
+		$("#nomePrincipal").html(data[indexUser].dados.nome + " "+ data[indexUser].dados.sobrenome);
+		$("#anos").html("Anos vividos: "+ data[indexUser].dados.idade);
+		$("#vivo").html("Dias vividos: "+ data[indexUser].dados.diasVividos);
+		
+	})
 }
