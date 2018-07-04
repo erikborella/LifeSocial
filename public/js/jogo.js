72de9668b5c7dd232ca36412ed17386ebd592321
ERIK PABLO SCHAEFFER BORELA <2017308229@LABINFO.CONCORDIA.IFC.EDU.BR>
2018-07-04 18:51:44 -0300

    js

--- public/js/jogo.js
+++ public/js/jogo.js
@@ -8,11 +8,23 @@
 
 	$(".gameTela").show();
 
+	var INTERVAL;
+
+	var idade;
+	var diasVividos;
+	var money
+
 	$.get("/getUserdata", (data, status) => {
+
+		idade = data[indexUser].dados.idade;
+		diasVividos = data[indexUser].dados.diasVividos;
+
 		$("#nomePrincipal").html(data[indexUser].dados.nome + " "+ data[indexUser].dados.sobrenome);
 		$("#anos").html("Anos vividos: "+ data[indexUser].dados.idade);
 		$("#vivo").html("Dias vividos: "+ data[indexUser].dados.diasVividos);
 		$("#money").html("Dinheiro: " + data[indexUser].dados.money);
+
+		passDay();
 
 	});
 }
@@ -20,5 +32,20 @@
 $("#fotoDiv").click(function() {
 	$("#file").click();
 });
+
+$("#option").click(function() {
+	stopDay();
+})
+
+
+function passDay() {
+	INTERVAL =  setInterval(() => {
+		console.log("test");
+	}, 30000);
+}
+
+function stopDay() {
+	clearInterval(INTERVAL);
+}
 
 
