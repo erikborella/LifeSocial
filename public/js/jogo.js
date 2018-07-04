a687d4e035447d1c5db0fc7d0dec9b9fdddb37da
ERIK PABLO SCHAEFFER BORELA <2017308229@LABINFO.CONCORDIA.IFC.EDU.BR>
2018-07-04 18:26:41 -0300

    Money

--- public/js/jogo.js
+++ public/js/jogo.js
@@ -12,11 +12,13 @@
 		$("#nomePrincipal").html(data[indexUser].dados.nome + " "+ data[indexUser].dados.sobrenome);
 		$("#anos").html("Anos vividos: "+ data[indexUser].dados.idade);
 		$("#vivo").html("Dias vividos: "+ data[indexUser].dados.diasVividos);
+		$("#money").html("Dinheiro: " + data[indexUser].dados.money);
 
-		
 	});
 }
 
 $("#fotoDiv").click(function() {
 	$("#file").click();
 });
+
+
