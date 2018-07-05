8c35f5ae3c17682ff01b93ea1a2e2742a21ae24d
ErikIPU2 <2017308229@LABINFO.CONCORDIA.IFC.EDU.BR>
2018-07-05 09:30:31 -0300

    Atualizar BD (Ainda não funciona)

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ mongoClient.connect("mongodb://localhost/Ftest")
             .then(conn => global.conn = conn.db("Ftest"))
             .catch(err => console.log(err))
 
-function findAll(callback){  
+function findAll(callback){
     global.conn.collection("userData").find({}).toArray(callback);
 }
 
@@ -11,4 +11,25 @@ function insert(customer, callback){
     global.conn.collection("userData").insert(customer, callback);
 }
 
-module.exports = { findAll, insert }
+function update(querry, data, callback) {
+  var adress = {"user" : querry};
+
+
+  var upd = {$set : {
+    "dados": {
+      "nome" : data.nome,
+      "sobrenome" : data.sobrenome,
+      "email": data.email,
+      "idade": data.idade,
+      "diasVividos": data.diasVividos,
+      "money": data.money
+    }
+  }}
+
+  console.log(upd);
+
+
+  // global.conn.collection("userData").updateOne(adress, upd, callback);
+}
+
+module.exports = { findAll, insert, update }
