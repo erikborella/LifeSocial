2f3ca2f54b2d21f74eee0c2055f97e7607e809cf
ErikIPU2 <erik02_palmeiras@hotmail.com>
2018-07-06 13:04:56 -0300

    Enter Login (Finalmente isso foi adicionado)

--- public/js/mainLogin.js
+++ public/js/mainLogin.js
@@ -1,63 +1,77 @@
+var userName;
+var inLogin = true;
+
 $(document).ready(function() {
 
 	$(".modal").modal({
 		dismissible	: false
 	});
 	$("#loginModal").modal("open");
-
-	var userName;
-
-	$("#loginbotao").click(function() {
-
-		$.get("/getUserdata", function(data, status) {
-
-			var userIndex;
-
-			userName = data;
-
-			var login = $("#logininput").val();
-			var senha = $("#senhainput").val();
-			if (!login) {
-				iziToast.error({
-					title: "Digite o seu username!",
-					position: "topRight"
-				});
-			}
-			else if (!senha) {
-				iziToast.error({
-					title: "Digite a sua Senha!",
-					position: "topRight"
-				});
-			}
-
-			else {
-				var acessou = false;
-
-				for (var i = 0; i < userName.length; i++) {
-					if (userName[i].user == login && userName[i].password == senha) {
-						iziToast.success({
-							title: "Bem vindo " + userName[i].user,
-							position: "topRight"
-						})
-						acessou = true;
-						userIndex = i;
-
-					}
-				}
-
-				if (!acessou) {
-					iziToast.error({
-						title: "Dados incorretos",
-						position: "topRight"
-					});
-					$("#lostPassword").show(100);
-				}
-				else {
-					initGame(userIndex);
-				}	
-			}
-		});	
-
-	});
 	$('.dropdown-trigger').dropdown();
+
+
 });
+
+$("#loginbotao").click(function() {
+	login();
+});
+
+$(document).keypress((key) => {
+	
+	if (inLogin && key.key === "Enter") {
+		login();
+	}
+})
+
+
+function login() {
+	
+	$.get("/getUserdata", function(data, status) {
+
+		var userIndex;
+
+		userName = data;
+
+		var login = $("#logininput").val();
+		var senha = $("#senhainput").val();
+		if (!login) {
+			iziToast.error({
+				title: "Digite o seu username!",
+				position: "topRight"
+			});
+		}
+		else if (!senha) {
+			iziToast.error({
+				title: "Digite a sua Senha!",
+				position: "topRight"
+			});
+		}
+
+		else {
+			var acessou = false;
+
+			for (var i = 0; i < userName.length; i++) {
+				if (userName[i].user == login && userName[i].password == senha) {
+					iziToast.success({
+						title: "Bem vindo " + userName[i].user,
+						position: "topRight"
+					})
+					acessou = true;
+					userIndex = i;
+
+				}
+			}
+
+			if (!acessou) {
+				iziToast.error({
+					title: "Dados incorretos",
+					position: "topRight"
+				});
+				$("#lostPassword").show(100);
+			}
+			else {
+				initGame(userIndex);
+			}	
+		}
+	});	
+}
