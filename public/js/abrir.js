1e8307704a1f13fed8e494c240a8f23b4c27cf90
BernardoFranceschina <Bernardofranceschina@gmail.com>
2018-07-27 22:55:00 -0300

    FIZ BOSOTA DNV MAS ESSA N TEM NADA A VER C A OUTRA SÓ ACHO QUE FIZ UM EQUIVOCO
    EQUIVOCO MUITO FODA

---
+++ public/js/abrir.js
@@ -0,0 +1,42 @@
+function esconderInicio() {
+    $("#inicio").hide();
+    
+    $("#comida").hide();
+    $("#saude").hide();
+    
+    $("#lixo").hide();
+    $("#fastfood").hide();
+    $("#hotdog").hide();
+    $("#casa").hide();
+    $("#restaurante").hide();
+    $("#chef").hide();
+
+    $("#lixorem").hide();
+    $("#casarem").hide();
+    $("#apronto").hide();
+    $("#clinica").hide();
+    $("#medic").hide();
+    $("#hospital").hide();
+}
+function esconderTrabalho() {
+    $("#trab").hide();
+    
+    $("#desonesto").hide();
+    $("#honesto").hide();
+    
+    $("#rdinheiro").hide();
+    $("#").hide();
+    $("#hotdog").hide();
+    $("#casa").hide();
+    $("#restaurante").hide();
+    $("#chef").hide();
+
+    $("#lixorem").hide();
+    $("#casarem").hide();
+    $("#apronto").hide();
+    $("#clinica").hide();
+    $("#medic").hide();
+    $("#hospital").hide();
+}
+
+
