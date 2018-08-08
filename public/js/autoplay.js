c000e131852eb310dc7e3f1b7794a47d29fb4b29
BernardoFranceschina <Bernardofranceschina@gmail.com>
2018-08-08 20:36:18 -0300

    Adicionado autoplay nos testemunhos do index e adicionado novas funções no jogo
    a

---
+++ public/js/autoplay.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:true
+});
+});
