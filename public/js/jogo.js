4281f1b350820e5be055b4111e8a78592364f5bb
ErikIPU2 <erik02_palmeiras@hotmail.com>
2018-08-25 18:57:28 -0300

    Tempo de jogo e adição dos dados de comida e remedios no DB

--- public/js/jogo.js
+++ public/js/jogo.js
@@ -1,5 +1,3 @@
-var data;
-
 $(document).ready(function() {
 
 	data = JSON.parse(localStorage.getItem("data"));
@@ -12,9 +10,46 @@ $(document).ready(function() {
 		iziToast.success({
 			title : "Bem vindo " + data.dados.nome
 		})
+
+		startGame(data);
 		
 	}
 })
+
+
+function startGame(data) {
+	var fome = data.gameValues.fome;
+	var saude = data.gameValues.saude;
+
+	var inteligencia = data.gameValues.inteligencia;
+	
+	var honestidade = data.gameValues.honestidade;
+
+	var imposto = data.gameValues.imposto;
+
+	var money = data.gameValues.money;
+	var diasVividos = data.gameValues.diasVividos;
+	var idade = data.gameValues.idade;
+
+	$("#money").html("Dinheiro : " + money);
+	$("#anos").html("Anos : " + idade);
+	$("#vivo").html("Dias vivos : " + diasVividos);
+
+	var gameTime = setInterval(function() {
+
+
+		diasVividos++;
+
+		if (diasVividos % 365 == 0) {
+			idade++;
+		}
+
+
+		$("#money").html("Dinheiro : " + money);
+		$("#anos").html("Anos : " + idade);
+		$("#vivo").html("Dias vivos : " + diasVividos);
+	}, 30);
+}
 
 $(window).bind("beforeunload", function() {
 	localStorage.removeItem("data");
