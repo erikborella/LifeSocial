c45d6bb245f64a6021433a042dd246c6bed02f65
BernardoFranceschina <Bernardofranceschina@gmail.com>
2018-08-26 15:30:53 -0300

    Identação e adionado modal pra segunda faculdade
    a

--- public/js/ezmodal.js
+++ public/js/ezmodal.js
@@ -1,31 +1,59 @@
-  //options
-  $(document).on('click', '.modal1', function(event) {
-    event.preventDefault();
-    $('#modal1').iziModal('open');
-  });
-  $('#modal1').iziModal({
-    headerColor: '#009688',
-    width: '50%', 
-    overlayColor: 'rgba(0, 0, 0, 0.5)',
-    openFullscreen: true,
-    transitionIn: 'fadeInUp',
-    transitionOut: 'fadeOutDown'
-  });
-  $('#modal2').iziModal({
-    headerColor: '#009688',
-    width: '50%', 
-    overlayColor: 'rgba(0, 0, 0, 0.5)',
-    openFullscreen: true,
-    transitionIn: 'fadeInUp',
-    transitionOut: 'fadeOutDown'
-  });
-  $('#modal3').iziModal({
-    headerColor: '#009688',
-    width: '50%', 
-    overlayColor: 'rgba(0, 0, 0, 0.5)',
-    openFullscreen: true,
-    transitionIn: 'fadeInUp',
-    transitionOut: 'fadeOutDown'
-  });
-  
- 
+//options
+$(document).on('click', '.modal1', function (event) {
+  event.preventDefault();
+  $('#modal1').iziModal('open');
+});
+$(document).on('click', '.modal4', function (event) {
+  event.preventDefault();
+  $('#modal4').iziModal('open');
+});
+$('#modal1').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+});
+$('#modal2').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+});
+$('#modal3').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+});
+$('#modal4').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+});
+$('#modal5').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+})
+$('#modal6').iziModal({
+  headerColor: '#009688',
+  width: '50%',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  openFullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
+})
+
+
